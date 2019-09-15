dae961ef878436fbbe8b347fc2f3573ec6213317
Ruster0046 <42268728+Ruster0046@users.noreply.github.com>
2019-09-15 21:03:08 +0200

    Helpers.cs
    Add the Helpers.cs to the Models folder.

---
+++ Helpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tax_Calculator.Models
+{
+    public class Helpers
+    {
+        public decimal Annual_salary { get; set; }
+        public string Postal_code { get; set; }
+        public decimal Tax_result { get; set; }
+        public string Tax_result_message { get; set; }
+        public string Error_message { get; set; }
+    }
+}
